22789a76440c7d24bb5d6d3691368e55355c355d
Rooooooooood <a2973799@gmail.com>
2024-01-25 09:26:56 +0800

    404

--- src/router/index.js
+++ src/router/index.js
@@ -104,6 +104,21 @@ const routes = [
         }
       }
     ]
+  },
+  {
+    path: '/404',
+    name: 'NotFound',
+    component: () => import('@/views/NotFoundView.vue'),
+    meta: {
+      title: '購物網 | 找不到',
+      login: false,
+      admin: false
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'All',
+    redirect: '/404'
   }
 ]
 
--- src/views/admin/HomeView.vue
+++ src/views/admin/HomeView.vue
@@ -2,5 +2,18 @@
 VContainer.h-100
   VRow.h-100(align="center")
     VCol(cols="12")
-      h1.text-center 請選擇管理項目
+      h1.text-center.text 請選擇管理項目
+      VBtn(@click="color = 'red'") red
+      VBtn(@click="color = 'blue'") blue
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const color = ref('red')
+
+</script>
+<style lang="sass">
+.text
+  color: v-bind(color)
+</style>
